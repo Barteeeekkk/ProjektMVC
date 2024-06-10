68a45d315fa04cc0328ceba68a493b5919cbe22e
Barteeeekkk <162622969+Barteeeekkk@users.noreply.github.com>
2024-06-10 16:37:54 +0200

    Delete routes/deleteForm.js

--- routes/deleteForm.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const formController = require('../controllers/formController');
-
-router.delete('/:id',formController.deleteFrom);
-
-module.exports = router;
